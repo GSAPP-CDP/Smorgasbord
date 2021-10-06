4276c4797c312fc7246bf284c9cc6e6f22d563c2
Dan Taeyoung <dan@dantaeyoung.com>
2021-10-06 17:32:33 -0400

    adding ignore-loader for markdown so that npm run build will work

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -50,5 +50,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend(config, { isDev, isClient }) {
+      config.module.rules.push({
+        test: /\.md$/i,
+        loader: 'ignore-loader'
+      })
+    }
   }
 }
