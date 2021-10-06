797561fa67b21d561f8611fd82eb4781bce9c2c1
Dan Taeyoung <dan@dantaeyoung.com>
2021-10-06 14:12:34 -0400

    run build update

--- src/.nuxt/components/index.js
+++ src/.nuxt/components/index.js
@@ -1,5 +1,5 @@
-export { default as NuxtLogo } from '../../components/NuxtLogo.vue'
-export { default as Tutorial } from '../../components/Tutorial.vue'
+export const NuxtLogo = () => import('../../components/NuxtLogo.vue' /* webpackChunkName: "components/nuxt-logo" */).then(c => wrapFunctional(c.default || c))
+export const Tutorial = () => import('../../components/Tutorial.vue' /* webpackChunkName: "components/tutorial" */).then(c => wrapFunctional(c.default || c))
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
