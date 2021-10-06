85d8eddc5d887451504c695854b38883af41ddb2
Dan Taeyoung <dan@dantaeyoung.com>
2021-10-06 14:13:46 -0400

    updatE

--- src/.nuxt/components/index.js
+++ src/.nuxt/components/index.js
@@ -1,5 +1,5 @@
-export const NuxtLogo = () => import('../../components/NuxtLogo.vue' /* webpackChunkName: "components/nuxt-logo" */).then(c => wrapFunctional(c.default || c))
-export const Tutorial = () => import('../../components/Tutorial.vue' /* webpackChunkName: "components/tutorial" */).then(c => wrapFunctional(c.default || c))
+export { default as NuxtLogo } from '../../components/NuxtLogo.vue'
+export { default as Tutorial } from '../../components/Tutorial.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
