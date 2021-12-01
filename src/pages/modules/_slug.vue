7f453d8b3e2caa5b0d35ff37997d22d1d58bb7d4
Dan Taeyoung <dan@dantaeyoung.com>
2021-12-01 13:47:13 -0500

    moving module to separate component

--- src/pages/modules/_slug.vue
+++ src/pages/modules/_slug.vue
@@ -1,15 +1,7 @@
 <template>
   <div class="post">
 
-    <div class="header">
-    </div>
-    <div class="body">
-      <nuxt-content :document="module" />
-    </div>
-    <div class="footer">
-      {{ module.date }}
-    </div>
-
+    <Module :slug="slug" />
     
     
   </div>
@@ -18,86 +10,19 @@
 
 <style>
 
-.post {
-  font-size: 0.95em;
-  font-weight: regular;
-  margin: auto;
-  width: 50vw;
-  max-width: 700px;
-  color: #444;
-}
-p {
-  line-height: 1.6em;
-}
-
-blockquote {
-  font-size: 0.9em;
-  line-height: 1.8em;
-  color: #666;
-
-}
-
-a {
-  color: #529F98
-}
-
-a:visited {
-  color: #6D71BF;
-}
-
-
-.title {
-  font-weight: bold;
-}
-
-
-img {
-  width: 100%;
-}
-
-img[src$='#img-left'] { 
-  float:left;
-  width: 50%;
-  padding: 10px;
-}
-
-img[src$='#img-right'] { 
-  float:right;
-  width: 50%;
-  padding: 10px;
-}
-
-.instruction {
-  background-color: #CCC;
-  border-radius: 10px;
-  padding: 20px 10px;
-}
-
-.instruction p:last-child {
-  margin-bottom: 0px;
-}
-
-/* image caption */
-img + em, div.img + em {
-  color: #888;
-  margin-bottom: 10px;
-  display: inline-block;
-}
-
-
-
 </style>
 
 <script>
 export default {
-  async asyncData({ $content, app, params, error }) {
-    const [module] = await $content({ deep: true }).where({ slug: params.slug }).fetch();
-
-    if (!module) {
-      return error({ statusCode: 404, message: 'Module not found' })
-    }
-    return { module: module };
+  data () {
+    return {
+      module: {},
+    };
   },
+  async asyncData({ params }) {
+    const slug = params.slug;
+    return { slug }
+  }
 }
 </script>
 
