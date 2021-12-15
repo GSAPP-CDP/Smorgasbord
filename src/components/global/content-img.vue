a7957051516711900b01e12f0f2b6ab9254946ad
Dan Taeyoung <dan@dantaeyoung.com>
2021-12-15 11:55:43 -0500

    content img should be working for deploy

--- src/components/global/content-img.vue
+++ src/components/global/content-img.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="img">
+    {{ src }} 
     <img :src="imgSrc()" :alt="alt" />
   </div>
 </template>
@@ -25,7 +26,22 @@ export default {
       // this is so gross..... but the only way to do it
 
       try {
+
+/*        console.log(this.dir);
+        console.log(this.src);
         const moduledir = this.$parent.$parent.$data.module.dir;
+        console.log("xo");
+        console.log(moduledir);
+        console.log("x");
+        console.log(this.$nuxt.$route.name);
+        console.log("yo"); */
+
+        var thispath = this.$nuxt.$route.path;
+        var thisdir = thispath.substr(0, thispath.lastIndexOf("/"))
+
+        //console.log("##########")
+//        console.log(thisdir);
+  
 
         var srconly = this.src.split('#')[0]
         var srchash = "";
@@ -33,7 +49,7 @@ export default {
           srchash = "#" + this.src.split("#")[1];
         }
         
-        return require(`~/content${moduledir}/${srconly}`) + srchash;
+        return require(`~/content${thisdir}/${srconly}`) + srchash;
       } catch (error) {
         return null;
       }
