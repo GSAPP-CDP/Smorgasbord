f6cb647db38495666919b7c0eda5fa3c5207fb36
Dan Taeyoung <dan@dantaeyoung.com>
2022-03-31 02:18:52 -0400

    module index is manually written

--- src/components/ModuleIndex.vue
+++ src/components/ModuleIndex.vue
@@ -1,91 +1,69 @@
 <template>
   <div class="moduleindex" :class="{ indexview: index }">
-    <ul v-for="sequence in contentdata.sequences" :class="{ sequence: true, hover: sequence.path == hover }" @mouseover="hover = sequence.path" @mouseleave="hover = false">
-      <div class="sequencetitle">{{ sequence.contentdata.title }}</div>
-      <div class="sequencemodules">
-        <div v-for="module in sequence.modules" :class="[ module.contentdata.path === $nuxt.$route.path ? 'active' : '']" >
-          <NuxtLink :to="module.contentdata.path"><span class="bullet" /> {{ module.contentdata.title }}</NuxtLink>
-        </div>
-      </div>
-    </ul>
 
+    <div class="course">
+      <div class="coursetitle">Mapping and Data</div>
+
+      <SequenceLinks path="14-geospatial-concepts" />
+      <SequenceLinks path="15-intro-qgis" />
+    </div>
+
+    <div class="course">
+      <div class="coursetitle">Architectural Drawing</div>
+
+      <SequenceLinks path="12-rhino-intro" />
+      <SequenceLinks path="13-arch-drawing" />
+    </div>
+
+    <div class="course">
+      <div class="coursetitle">Programming for Design Practices: A - Grasshopper</div>
+
+      <SequenceLinks path="4-grasshopper-intro" />
+      <SequenceLinks path="5-computational-design-modeling-in-grasshopper" />
+    </div>
+
+    <div class="course">
+      <div class="coursetitle">Programming for Design Practices: B - Python</div>
+
+      <SequenceLinks path="6-intro-to-python" />
+      <SequenceLinks path="7-fundamentals-of-python" />
+      <SequenceLinks path="8-intro-to-python-in-rhino" />
+      <SequenceLinks path="10-spatial-python" />
+    </div>
+
+    <div class="course">
+      <div class="coursetitle">Programming for Design Practices: C - Web and Interactivity</div>
+
+      <SequenceLinks path="11-interactive-web" />
+      <SequenceLinks path="16-ambient-computing-network-architecture" />
+      <SequenceLinks path="17-Ambient-computing-interactivity" />
+      <SequenceLinks path="18-Behavioral-Modeling-Intro" />
+    </div> 
 
   </div>
 </template>
 
 <style scoped>
 
-.active, .active > *  {
-  color: #4CBF8F !important; 
-  font-weight: bold;
+.course {
 }
+
+.coursetitle {
+  font-size: 2em;
+  margin: 30px 0px;
+}
+
 .moduleindex {
   color: 555;
   font-family: Lato;
   color: black;
+}
+
+.sequence {
   line-height: 1.5em;
+  margin-bottom: 20px;
 }
 
-.hover.sequence  {
-  color: #0096EA !important;
-}
-
-a {
-  text-decoration: none;
-}
-
-a:hover {
-  color: #0096EA !important;
-}
-
-
-
-ul {
-  padding: 0px 0px 0px 10px;
-  list-style-type: none;
-}
-
-.bullet::before {
-  content: "•";
-}
-
-.sequencetitle {
-  font-weight: bold;
-  font-size: 1.15em;
-}
-
-.indexview .bullet {
-}
-
-
-.indexview.moduleindex {
-  font-size: 1.25em;
-}
-
-.indexview .sequence {
-  display: flex;
-  flex-direction: row;
-  border-top: 2px solid #555;
-  padding-top: 5px;
-  margin-bottom: 5px;
-}
-
-.indexview .sequence.hover {
-  border-top: 2px solid #0096EA ;
-}
-
-.indexview .sequencetitle {
-  width: 50%;
-  padding-right: 20px;
-}
-.indexview .sequencemodules {
-  width: 50%;
-  font-style: italics;
-}
-
-.indexview .ul {
-  padding-left: 0px;
-}
 
 </style>
 
---
+++ src/components/SequenceLinks.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="sequence" v-if="sequencedata !== null">
+
+      <div class="sequencetitle">{{ sequencedata.contentdata.title }}</div>
+      <div class="sequencemodules">
+        <div v-for="module in sequencedata.modules" :class="[ module.contentdata.path === $nuxt.$route.path ? 'active' : '']" >
+          <NuxtLink :to="module.contentdata.path"><span class="bullet" /> {{ module.contentdata.title }}</NuxtLink>
+        </div>
+      </div>
+
+
+
+  </div>
+</template>
+
+<style scoped>
+
+.active, .active > *  {
+  color: #4CBF8F !important; 
+  font-weight: bold;
+}
+.moduleindex {
+  color: 555;
+  font-family: Lato;
+  color: black;
+  line-height: 1.5em;
+}
+
+.hover.sequence  {
+  color: #0096EA !important;
+}
+
+a {
+  text-decoration: none;
+}
+
+a:hover {
+  color: #0096EA !important;
+}
+
+
+
+ul {
+  padding: 0px 0px 0px 10px;
+  list-style-type: none;
+}
+
+.bullet::before {
+  content: "•";
+}
+
+.sequencetitle {
+  font-weight: bold;
+  font-size: 1.15em;
+}
+
+.indexview .bullet {
+}
+
+
+.indexview.moduleindex {
+  font-size: 1.25em;
+}
+
+.indexview .sequence {
+  display: flex;
+  flex-direction: row;
+  border-top: 2px solid #555;
+  padding-top: 5px;
+  margin-bottom: 5px;
+}
+
+.indexview .sequence.hover {
+  border-top: 2px solid #0096EA ;
+}
+
+.indexview .sequencetitle {
+  width: 50%;
+  padding-right: 20px;
+}
+.indexview .sequencemodules {
+  width: 50%;
+  font-style: italics;
+}
+
+.indexview .ul {
+  padding-left: 0px;
+}
+
+</style>
+
+<script>
+
+export default {
+  data() {
+    return {
+      hover: false,
+    };
+  }, 
+  props: ['path'],
+  computed: {
+    contentdata () {
+      return this.$store.state.contentdata
+    },
+    sequencedata() {
+      var self = this;
+      var f = self.contentdata.sequences.filter(d => {
+        return d.path == self.path;
+      })
+      if(f.length > 0) { return f[0]; }
+      else { return null; }
+    },
+  },
+  methods: {
+  },
+}
+</script>
