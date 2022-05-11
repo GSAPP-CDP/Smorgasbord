757838ced2f0f741935932a7cca3dfde9eaa700e
Dan Taeyoung <dan@dantaeyoung.com>
2022-05-11 19:46:11 -0400

    only one layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -9,7 +9,40 @@
   </div>
 </template>
 
-<style scoped>
+
+<style >
+#default {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: #F2EDE7;
+  font-family: "IBM Plex Sans", Helvetica, sans-serif;
+}
+a, a:visited {
+  color: #555;
+}
+#default > * {
+}
+#nav-column {
+  padding: 20px 30px;
+  overflow-y: auto;
+  flex-grow: 0;
+  flex-shrink: 0;
+  width: 90vw;
+  max-width: 900px; 
+} 
+.moduleindex {
+}
+  
+.introblurb {
+  margin-bottom: 20px;
+  max-width: 600px;
+}
+#graph-column {
+  display: none;
+}
+
 #layout-module {
   display: flex;
   width: 100%;
