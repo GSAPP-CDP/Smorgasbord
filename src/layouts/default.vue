12e043194f11fcb61f91ccaf8547f7e10b180e1a
Dan Taeyoung <dan@dantaeyoung.com>
2022-05-11 19:42:54 -0400

    getting rid of _.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,20 +1,19 @@
 <template>
-  <div id="default">
+  <div id="layout-module">
     <div id="nav-column">
-      <Logo width="300px"/>
-      <Copy class="introblurb" copy="introblurb" />
-      <div id="nav"><Nav index="true"/></div>
+      <div id="logo"><Logo width="220px"/></div>
+      <div id="nav"><Nav /></div>
     </div>
+    <div id="content-column"><Nuxt /><Copyright /></div>
     <div id="graph-column"><Graph /></div>
   </div>
 </template>
 
-<style >
-#default {
+<style scoped>
+#layout-module {
   display: flex;
-  flex-direction: column;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   background-color: #F2EDE7;
   font-family: "IBM Plex Sans", Helvetica, sans-serif;
 }
@@ -28,26 +27,32 @@ a, a:visited {
 }
 
 #nav-column {
-  padding: 20px 30px;
   overflow-y: auto;
+  flex-basis: 350px;
   flex-grow: 0;
   flex-shrink: 0;
-  width: 90vw;
-  max-width: 900px; 
-} 
-
-.moduleindex {
+  padding: 20px 30px;
 }
 
-  
+#logo {
+  height: 100px;
+}
 
-.introblurb {
-  margin-bottom: 20px;
-  max-width: 600px;
+#content-column {
+  overflow-y: auto;
+  width: 100%;
 }
 
 #graph-column {
   display: none;
 }
 </style>
+
+<script>
+export default {
+  scrollToTop: true,
+}
+
+</script>
+
 
